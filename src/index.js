52a10f016121290b878548365fae80a2aab3101b
Olavio Lacerda <olavio.lacerda@hotmail.com>
2018-01-17 14:49:34 -0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import {
 
 ReactDOM.render(
     (
-        <Router history={browserHistory}>
+        <Router>
             <Route path="/" component={App}>
                 <IndexRoute component={Home}/>
                 <Route path="/gallery" component={Gallery} />
