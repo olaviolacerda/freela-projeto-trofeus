7113a566ba3a4b879c26a33b2984eb21a7949cf0
olaviolacerda <olavio.lacerda@hotmail.com>
2018-04-04 01:36:58 -0300

    fixed error on enter page

--- src/helpers/Helper.js
+++ src/helpers/Helper.js
@@ -1,5 +1,5 @@
-export const capitalize = (str) => `${str.charAt(0).toUpperCase()+str.slice(1)}`; 
+export const capitalize = (str) => `${str.charAt(0).toUpperCase() + str.slice(1)}`;
 
 export const background = (image) => `url(${image})`;
 
-export const clearFields = (matches) => matches.forEach(match => { match.value = ''});
+export const clearFields = (matches) => matches.forEach(match => { match.value = '' });
