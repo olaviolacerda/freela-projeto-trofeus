d0ba6ce175ce0ed57ce67c0e98533f5c88fd7470
olaviolacerda <olavio.lacerda@hotmail.com>
2018-04-05 00:10:07 -0300

    added google maps

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
+import MapComponent from '../components/MapComponent';
+
+import '../css/Footer.scss';
 
 export default class Footer extends Component {
     render() {
         return (
-            <footer id="" role="">
+            <footer id="footer" className="footer" role="footer">
 
-                <div className="align-center">
+                <div className="row">
+                    <div className="col-6 left">
+                        <p >A Pablo Gravações trabalha há mais de 20 anos no mercado de gravação em metal, produzindo placas, bottons e troféus de alta qualidade.</p>
+                    </div>
+                    <div className="col-6 right">
+                        <MapComponent />
+                    </div>
                     <div className="row">
-                        <div className="row">
-                            <div className="col-10 col-2-fill">
-                                <h3>About Us</h3>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-                            </div>
-                        </div>
-                        <div className="row">
-
-                            <div className="col-10 col-2-fill">
-                                <p>&copy; 2016 Free HTML5 template. All Rights Reserved. <span>Designed with <i className="icon-heart"></i> by <a href="http://freehtml5.co/" target="_blank" rel="noopener noreferrer">FreeHTML5.co</a> Demo Images by <a href="http://unsplash.com/" target="_blank" rel="noopener noreferrer">Unsplash</a></span></p>
-                            </div>
-                        </div>
+                        <p className=" signature"> Desenvolvido por <a href="https://github.com/olaviolacerda" rel="noopener noreferrer" target="_blank">Olavio Lacerda</a><span className="copy">&copy;</span></p>
                     </div>
                 </div>
+
             </footer>
         );
     }
